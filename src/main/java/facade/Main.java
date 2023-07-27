78727bb6bf3f3d14464f5dd2e21302c48c2092e4
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-07-27 10:35:37 +0300

    splitting classes into files

--- src/main/java/facade/Main.java
+++ src/main/java/facade/Main.java
@@ -1,15 +1,5 @@
 package facade;
 
-class BinOps {
-    public String sum(String a, String b) {
-        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
-    }
-
-    public String mult(String a, String b) {
-        return Integer.toBinaryString(Integer.parseInt(a, 2) * Integer.parseInt(b, 2));
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         BinOps bins = new BinOps();
